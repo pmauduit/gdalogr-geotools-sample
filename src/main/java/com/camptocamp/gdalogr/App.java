d1db4a99cf77eafcbbffbcf48d3133d416bff539
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2014-04-10 15:25:27 +0200

    Modifying App to make more thorough setup debug tests

--- src/main/java/com/camptocamp/gdalogr/App.java
+++ src/main/java/com/camptocamp/gdalogr/App.java
@@ -1,49 +1,43 @@
 package com.camptocamp.gdalogr;
-import java.util.Iterator;
-import java.util.Set;
 
-import org.geotools.data.DataStoreFinder;
+import java.util.Iterator;
+
 import org.geotools.data.DataStoreFactorySpi;
-import org.geotools.data.ogr.OGR;
-import org.geotools.data.ogr.OGRDataStore;
+import org.geotools.data.DataStoreFinder;
 import org.geotools.data.ogr.OGRDataStoreFactory;
+import org.geotools.data.ogr.jni.JniOGR;
 import org.geotools.data.ogr.jni.JniOGRDataStoreFactory;
 
-
-public class App 
-{
-  public static void main( String[] args )
-  {
-    // Dumping available datastores
-    boolean containsOGR = false;
-    System.out.println("[GDAL/OGR-SAMPLE] Available datastores:");
-    Iterator<DataStoreFactorySpi> dtf =  DataStoreFinder.getAllDataStores();
-    while (dtf.hasNext()) {
-      DataStoreFactorySpi dsfspi = dtf.next();
-      System.out.println("\t" + dsfspi.getDisplayName());
-      if ("OGR".equalsIgnoreCase(dsfspi.getDisplayName()))
-        containsOGR = true;
-    }
-    if (! containsOGR) {
-      System.out.println("[GDAL/OGR-SAMPLE] OGR Datastore could not be found.");
-    }
-    OGRDataStoreFactory f = new JniOGRDataStoreFactory();
-    
-    if (f.isAvailable()) {
-        System.out.println("[GDAL/OGR-SAMPLE] OGR Datastore available.");
-    } else {
-        System.out.println("[GDAL/OGR-SAMPLE] OGR Datastore unavailable.");
-    }
-    // Dumping OGR drivers (should fail if unable to load the ogrjni library)
-    System.out.println("[GDAL/OGR-SAMPLE] trying to enumerate OGR drivers.");
-    try {
-      OGRDataStoreFactory factory = new JniOGRDataStoreFactory();
-      Set<String> drivers = factory.getAvailableDrivers();
-      for (String driver : drivers) {
-        System.out.println("\tdriver: "+ driver);
-      }
-    } catch (UnsatisfiedLinkError e) {
-      System.out.println("[GDAL/OGR-SAMPLE] Error: Check java.library.path.");
-    }
-  }
+public class App {
+	public static void main(String[] args) {
+		// Dumping available datastores
+		boolean containsOGR = false;
+		System.out.println("[GDAL/OGR-SAMPLE] Available datastores:");
+		Iterator<DataStoreFactorySpi> dtf = DataStoreFinder.getAllDataStores();
+		while (dtf.hasNext()) {
+			DataStoreFactorySpi dsfspi = dtf.next();
+			System.out.println("\t" + dsfspi.getDisplayName());
+			if ("OGR".equalsIgnoreCase(dsfspi.getDisplayName()))
+				containsOGR = true;
+		}
+		if (!containsOGR) {
+			System.out
+					.println("[GDAL/OGR-SAMPLE] OGR Datastore could not be found.");
+			System.out
+					.println("[GDAL/OGR-SAMPLE] Trying to get an OGR Datastore via JNI anyway ...");
+		}
+		OGRDataStoreFactory f = new JniOGRDataStoreFactory();
+		if (f.isAvailable()) {
+			System.out
+					.println("[GDAL/OGR-SAMPLE] OGR Datastore available. Available drivers:");
+			for (String s : f.getAvailableDrivers()) {
+				System.out.println("\tdriver: " + s);
+			}
+		} else {
+			System.out.println("[GDAL/OGR-SAMPLE] OGR Datastore unavailable.");
+		}
+		JniOGR jo = new JniOGR();
+		System.out.print("[GDAL/OGR-SAMPLE] Count " + jo.GetDriverCount()
+				+ " drivers.\n");
+	}
 }
